985cca1559c865f3735043147fab0a611b6f5661
PG_YShimohara <shimohara@platinumgames.co.jp>
2023-04-04 18:01:37 +0900

    test

--- pull_and_rmdir2.py
+++ pull_and_rmdir2.py
@@ -3,16 +3,15 @@ from git import Repo
 import os
 import shutil
 
-OUTPUT_PATH = 'mdouttest/055-master-data/master/p1/sheet_work/'
-REPOSITORY_PATH = 'mdouttest'
+OUTPUT_PATH = '055-master-data/master/p1/sheet_work/'
+REPOSITORY_PATH = '055-master-data'
 
-full_local_path = "mdouttest"
-remote = f"https://{os.environ['GIT_USERNAME']}:{os.environ['GIT_PASSWORD']}@github.com/shadelurk/mdouttest.git"
+remote = f"https://{os.environ['GIT_USERNAME']}:{os.environ['GIT_PASSWORD']}@github.com/PlatinumGames-Inc/055-master-data.git"
 
 if os.path.exists(REPOSITORY_PATH) != True:
-    Repo.clone_from(remote,full_local_path)
+    Repo.clone_from(remote,REPOSITORY_PATH)
 
-g = git.cmd.Git(full_local_path)
+g = git.cmd.Git(REPOSITORY_PATH)
 g.pull()
 
 shutil.rmtree(OUTPUT_PATH)
