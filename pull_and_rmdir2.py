6622549c2f1a3b5e6a5022067a99fd85423b2320
PG_YShimohara <shimohara@platinumgames.co.jp>
2023-04-04 16:33:05 +0900

    test

--- pull_and_rmdir2.py
+++ pull_and_rmdir2.py
@@ -6,10 +6,15 @@ GIT_REPOSITORY = 'https://github.com/shadelurk/mdouttest.git'
 OUTPUT_PATH = '055-master-data/master/p1/sheet_work/'
 REPOSITORY_PATH = 'mdouttest'
 
+credentials = os.environ['GIT_CREDENTIALS']
+
+# リポジトリをクローン
+
 if os.path.exists(REPOSITORY_PATH) != True:
     git.Repo.clone_from(
-        GIT_REPOSITORY,
-        REPOSITORY_PATH)
+        GIT_REPOSITORY, 
+        REPOSITORY_PATH, 
+        env={'GIT_ASKPASS': '/bin/echo', 'GIT_USERNAME': credentials.username, 'GIT_PASSWORD': credentials.password})
 
 g = git.cmd.Git(REPOSITORY_PATH)
 g.pull()
