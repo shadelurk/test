ab993521b8e3f9548551b49b232b7a9916bb6688
PG_YShimohara <shimohara@platinumgames.co.jp>
2023-04-04 17:04:51 +0900

    test

--- pull_and_rmdir2.py
+++ pull_and_rmdir2.py
@@ -2,15 +2,13 @@ import git
 import os
 import shutil
 
-GIT_REPOSITORY = 'https://github.com/shadelurk/mdouttest.git'
-OUTPUT_PATH = '055-master-data/master/p1/sheet_work/'
+GIT_URL = 'https://github.com/shadelurk/mdouttest.git'
+GIT_TOKEN = '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'
 REPOSITORY_PATH = 'mdouttest'
+OUTPUT_PATH = '055-master-data/master/p1/sheet_work/'
 
 if os.path.exists(REPOSITORY_PATH) != True:
-    git.Repo.clone_from(
-        GIT_REPOSITORY, 
-        REPOSITORY_PATH, 
-        env={'GIT_ASKPASS': '/bin/echo', 'GIT_USERNAME': os.environ['GIT_USERNAME'], 'GIT_PASSWORD': os.environ['GIT_PASSWORD']})
+    git.Repo.clone_from(GIT_URL, OUTPUT_PATH, branch='main', depth=1, auth=git.auth.HttpGitAuth('token', GIT_TOKEN))
 
 g = git.cmd.Git(REPOSITORY_PATH)
 g.pull()
