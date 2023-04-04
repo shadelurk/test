c0a552a29852fd5bd75d48888edb14f65eb0145e
PG_YShimohara <shimohara@platinumgames.co.jp>
2023-04-04 17:40:24 +0900

    test

--- pull_and_rmdir2.py
+++ pull_and_rmdir2.py
@@ -4,7 +4,7 @@ import os
 import shutil
 
 GIT_REPOSITORY = 'https://github.com/PlatinumGames-Inc/055-master-data.git'
-OUTPUT_PATH = '055-master-data/master/p1/sheet_work/'
+OUTPUT_PATH = 'mdouttest/055-master-data/master/p1/sheet_work/'
 REPOSITORY_PATH = 'mdouttest'
 
 full_local_path = "mdouttest"
