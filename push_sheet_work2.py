a900d6f4a920dc071cd09948f16003ba0452e885
PG_YShimohara <shimohara@platinumgames.co.jp>
2023-04-05 13:29:04 +0900

    test

--- push_sheet_work2.py
+++ push_sheet_work2.py
@@ -1,9 +1,9 @@
 import git
 import os
 
-GIT_REPOSITORY = 'https://github.com/shadelurk/mdouttest.git'
+GIT_REPOSITORY = 'https://github.com/PlatinumGames-Inc/055-master-data.git'
 OUTPUT_PATH = '055-master-data/master/p1/sheet_work/'
-REPOSITORY_PATH = 'mdouttest'
+REPOSITORY_PATH = '055-master-data'
 
 os.chdir(REPOSITORY_PATH)
 repo = git.Repo()
