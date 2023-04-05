b25e22028e11f50fe39245ad1dbf39e469e182fb
PG_YShimohara <shimohara@platinumgames.co.jp>
2023-04-05 14:02:47 +0900

    test

--- push_sheet_work2.py
+++ push_sheet_work2.py
@@ -6,6 +6,10 @@ OUTPUT_PATH = '055-master-data/master/p1/sheet_work/'
 REPOSITORY_PATH = '055-master-data'
 
 os.chdir(REPOSITORY_PATH)
+os.environ['GIT_AUTHOR_NAME'] = 'PRJ055Jenkins'
+os.environ['GIT_AUTHOR_EMAIL'] = 'prj055_appsheet@platinumgames.co.jp'
+os.environ['GIT_COMMITTER_NAME'] = 'PRJ055Jenkins'
+os.environ['GIT_COMMITTER_EMAIL'] = 'prj055_appsheet@platinumgames.co.jp'
 repo = git.Repo()
 o = repo.remotes.origin
 o.pull()
