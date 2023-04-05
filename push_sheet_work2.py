705e5beb7adfd8be10f46fb838d2177abc0a112d
PG_YShimohara <shimohara@platinumgames.co.jp>
2023-04-05 14:00:37 +0900

    test

--- push_sheet_work2.py
+++ push_sheet_work2.py
@@ -12,4 +12,4 @@ o = repo.remotes.origin
 o.pull()
 repo.git.add('--all')
 repo.git.commit('.','-m','\"GAS auto commit\"')
-repo.git.push('origin', 'main', env={'GIT_USERNAME': $GIT_USERNAME, 'GIT_PASSWORD': $GIT_PASSWORD})
+repo.git.push('origin', 'main', env={'GIT_USERNAME': os.environ['GIT_USERNAME'], 'GIT_PASSWORD': os.environ['GIT_PASSWORD']})
