8f25bc88a9f9c68247044fbdcfbf61cba0f519a3
v-shimohara <v-shimohara@N2210013.odd-inc.local>
2023-06-01 12:04:32 +0900

    test

--- bat_egt_generate.py
+++ bat_egt_generate.py
@@ -3,5 +3,5 @@ import os
 
 os.system('pwd')
 os.system('docker-compose --version')
-os.system('curl 169.254.169.254/latest/meta-data/instance-id/')
+os.system('/usr/local/bin/docker-compose --version')
 os.system('bash 055-server/bat/egt_generate.sh')
