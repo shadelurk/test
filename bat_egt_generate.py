3b8cc8978eed10a40b7e4f4c05c583650f91360a
v-shimohara <v-shimohara@N2210013.odd-inc.local>
2023-06-01 13:59:10 +0900

    test

--- bat_egt_generate.py
+++ bat_egt_generate.py
@@ -1,12 +1,5 @@
 import git
 import os
-import docker
-
-client = docker.from_env(base_url='unix://var/run/docker.sock')
-client.df()
-client.info()
-client.version()
-client.ping()
 
 os.system('pwd')
 os.system('docker-compose --version')
