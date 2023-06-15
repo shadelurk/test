3b39014484c8de3d6917625c2808824a7e3fe3f9
YasutakaShimoharaPG <shimohara@platinumgames.co.jp>
2023-06-15 16:50:33 +0900

    test

--- sheet_csv_download.py
+++ sheet_csv_download.py
@@ -13,7 +13,7 @@ from googleapiclient.http import MediaIoBaseDownload
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 FOLDER_ID = '1Xu8vMYSV0J57hAXaK1fFmNLlU4861dvs'
-OUTPUT_PATH = './'
+OUTPUT_PATH = './055-master-data/output/csv/'
 
 def main():
     # OAuth
